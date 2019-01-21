a86b89c37f91c29e22690f6018e1425ad9ad9dfe
Vinh Do <vinhdo07@gmail.com>
2019-01-21 20:08:46 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -25,14 +25,14 @@ def reformat_languages(languages)
   languages.each do |style_key, lang_val|
     lang_val.each do |lang_key, lang_attr|
       new_hash[lang_key] = lang_attr
-    end
 
-    new_hash.each do |hash_key, hash_val|
-      if style_key == :oo && lang_key == hash_key
-        new_hash[:style] = [style_key]
-        binding.pry
-      elsif style_key == :functional && lang_key == hash_key
-        new_hash[:style] = [style_key]
+      new_hash.each do |hash_key, hash_val|
+        if style_key == :oo && lang_key == hash_key
+          new_hash[:style] = [style_key]
+          binding.pry
+        elsif style_key == :functional && lang_key == hash_key
+          new_hash[:style] = [style_key]
+        end
       end
     end
   end
